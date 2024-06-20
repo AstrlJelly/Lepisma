49f521ac74b87c8e8e408db93011aa880093ef82
AstrlJelly <bdlawson115@gmail.com>
2024-06-20 15:59:07 -0400

    init project
    probs won't go anywhere with this for now

---
+++ src/client/kotlin/astrljelly/lepisma/LepismaClient.kt
@@ -0,0 +1,9 @@
+package astrljelly.lepisma
+
+import net.fabricmc.api.ClientModInitializer
+
+object LepismaClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/astrljelly/lepisma/LepismaDataGenerator.kt
@@ -0,0 +1,9 @@
+package astrljelly.lepisma
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object LepismaDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
